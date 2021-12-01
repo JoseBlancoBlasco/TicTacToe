e12b4e3fa1f2e7896a048dfa6367c122ce3cc7e0
Giuseppe <giuseppeanimation@gmail.com>
2021-12-01 16:30:43 +0100

    Prueba cambiando el tablero a mostrar con las posiciones

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -14,31 +14,41 @@ public class TicTacToe {
             {' ', '|', ' ', '|', ' '}
         };
         
-        boolean turno;
+        char[][] tableroMuestra = {
+            {'1', '|', '2', '|', '3'},
+            {'-', '+', '-', '+', '-'},
+            {'4', '|', '5', '|', '6'},
+            {'-', '+', '-', '+', '-'},
+            {'7', '|', '8', '|', '9'}
+        };
+
+        boolean turno=true;
 
         /*Guia Visual Indices Jugables
             [0,0],[0,2],[0,4]
             [2,0],[2,2],[2,4]
             [4,0],[4,2],[4,4]
          */
-        mostrarTablero(tablero);
+        mostrarTablero(tableroMuestra);
 
         while (true) {
-            turno=true;
-            mostrarTurno(turno);
-            System.out.print("Introduce la posicion (1-9): ");
-            int posicionJugador1 = scan.nextInt();
-            ponerFicha(tablero, posicionJugador1, "Jugador 1");
-            mostrarTablero(tablero);            
-            //comprobarGanador();            
-                        
-            turno=false;
-            mostrarTurno(turno);
-            System.out.print("Introduce la posicion (1-9): ");
-            int posicionJugador2 = scan.nextInt();
-            ponerFicha(tablero, posicionJugador2, "Jugador 2");
-            mostrarTablero(tablero);
-            //comprobarGanador();
+            if (turno) {
+                mostrarTurno(turno);
+                System.out.print("Introduce la posicion (1-9): ");
+                int posicionJugador1 = scan.nextInt();
+                ponerFicha(tableroMuestra, posicionJugador1, "Jugador 1");
+                mostrarTablero(tableroMuestra);
+                turno=false;
+                //comprobarGanador();            
+            } else {                
+                mostrarTurno(turno);
+                System.out.print("Introduce la posicion (1-9): ");
+                int posicionJugador2 = scan.nextInt();
+                ponerFicha(tableroMuestra, posicionJugador2, "Jugador 2");
+                mostrarTablero(tableroMuestra);
+                turno=true;
+                //comprobarGanador();
+            }
         }
 
     }
@@ -94,17 +104,19 @@ public class TicTacToe {
         }
     }
 
-    public static void mostrarTurno(boolean turno) {
-        
+    public static boolean mostrarTurno(boolean turno) {
+
         if (turno) {
             System.out.println("Turno del Jugador 1.");
+            return turno=true;
         } else {
             System.out.println("Turno del Jugador 2.");
+            return turno=false;
         }
 
     }
-    
-    public static String comprobarGanador(){
+
+    public static String comprobarGanador() {
         return "";
     }
 
